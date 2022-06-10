8ed3a5044b1ad2d1c1109b1fd9bfe6ffc30db7f4
emiliyank <e.kadiyski@gmail.com>
2022-06-10 20:48:01 +0300

    Try to commit with TODO

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        //TODO: finish this feature
         System.out.println("Hello world!");
         System.out.println("Hi there!");
         System.out.println("a new line printed!");
